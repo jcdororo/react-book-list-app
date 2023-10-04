6ae0b605e4f03adf63532417a1734eae47a1e9da
jc <wnwlcks123@gmail.com>
2023-10-04 16:02:04 +0900

    Footer 컴포넌트 생성 및 Theme 적용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+/** @jsxImportSource @emotion/react */
+
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import BookDetailPage from "./pages/BookDetailPage";
-import { Global, css } from "@emotion/react";
+import { Global, ThemeProvider, css, useTheme } from "@emotion/react";
+import Footer from "./components/Footer";
+import { useState } from "react";
+import { themeDark, themeLight } from "./components/Theme";
 
-const Layout = () => {
+const Layout = ({isDark, setIsDark}) => {
+  const theme = useTheme();
+  console.log('theme',theme)
+
+
   return (
     <div>
       <Global 
         styles={css`
           body {
-            background-color: white;
-            color: black;
+            background-color: ${theme.background};
+            color: ${theme.text};
             transition-duration: 0.2s;
             transition-property: background-color, color;
           }
           a {
-            color: black;
+            color: ${theme.text};
             text-decoration: none;
           }
           ul {
@@ -24,20 +33,33 @@ const Layout = () => {
           }
         `}
       />
+      <div
+        css={css`
+          min-height: 90vh;
+        `}
+      >
       <Outlet />
+
+      </div>
+      <Footer isDark={isDark} setIsDark={setIsDark} />
     </div>
   )
 }
 
 function App() {
+
+  const [isDark, setIsDark] = useState(false)
+
   return (
     <BrowserRouter>
+    <ThemeProvider theme={isDark ? themeDark : themeLight}>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Layout isDark={isDark} setIsDark={setIsDark} />}>
           <Route index element={<SearchPage />} />
           <Route path="/book/:bookId" element={<BookDetailPage />} />
         </Route>
       </Routes>
+    </ThemeProvider>
     </BrowserRouter>
   );
 }
